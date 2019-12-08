2d0e40e121b5ea1e11342f3b451306520fad054b
matijatomc <55134628+matijatomc@users.noreply.github.com>
2019-12-08 21:31:31 +0100

    Add files via upload

---
+++ tomcMatijaConvert/tomcMatijaConvert/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000;
+            byte b = (byte) i;
+
+            var number = "526";
+            int stringToInt = Convert.ToInt32(number);
+
+            try
+            {
+                byte stringToByte = Convert.ToByte(number);
+                Console.WriteLine(stringToByte);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ne radi. Nemože convertat string u byte");
+            }
+
+            try
+            {
+                string str = "true";
+                bool bo = Convert.ToBoolean(str);
+                Console.WriteLine(bo);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ne radi");
+            }
+
+            Console.WriteLine(b);
+            Console.WriteLine(stringToInt);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tomcMatijaDeklariranje/tomcMatijaDeklariranje/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaDeklariranje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.ReadKey();
+        }
+    }
+}
